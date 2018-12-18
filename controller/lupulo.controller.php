870a291720045d592affb161ca2e6ca05be80dae
andillas <andillas@yahoo.es>
2018-12-18 17:23:53 +0100

    Añadida página 404

--- controller/lupulo.controller.php
+++ controller/lupulo.controller.php
@@ -50,4 +50,40 @@ class LupuloController
         }
 
     }
+
+    public function editLuplo(){
+        try{
+            if(!$id_lupulo = filter_input(INPUT_GET, 'id_lupulo', FILTER_VALIDATE_INT)) throw new Exception('El id no es válido.');
+            $lupulo_data = $this->obj_lupulo->getLupuloById($id_lupulo);
+
+            require_once "view/fragments/header.php";
+            require_once "view/lupulo/edit_lupulo.php";
+            require_once "view/fragments/footer.php";
+
+        }catch(Exception $e){
+            Output::throwError($e->getMessage());
+        }
+    }
+
+    public function saveEditedLupulo(){
+        try{
+
+            if(!$id = filter_input(INPUT_POST, 'id_editar_lupulo', FILTER_VALIDATE_INT))throw new Exception('El id no es válido');
+            if(!$nombre = filter_input(INPUT_POST, 'nombre_editar_malta'))throw new Exception('El nombre no es válido');
+            if(!$tipo = filter_input(INPUT_POST, 'tipo_editar_malta'))throw new Exception('El tipo no es válido');
+            if(!$ebc = filter_input(INPUT_POST, 'ebc_editar_malta', FILTER_VALIDATE_FLOAT))throw new Exception('El valor de alfa ácidos no es válido.');
+            $notas = null;
+
+            $update = $this->obj_malta->updateMaltaById($id, $nombre, $tipo, $ebc, $notas);
+            if($update < 1){
+                Output::throwError('No ha sido posible actualizar la malta.' . $update);
+            }else{
+                Output::throwOk();
+            }
+
+
+        }catch(Exception $e){
+            Output::throwError($e->getMessage());
+        }
+    }
 }
--- view/lupulo/edit_lupulo.php
+++ view/lupulo/edit_lupulo.php
@@ -1,17 +1,18 @@
 <section class="container">
-    <header><h2>Nuevo lúpulo</h2></header>
+    <header><h2>Editar lúpulo</h2></header>
     <article>
         <form>
+            <input type="hidden" id="id_editar_lupulo" name="id_editar_lupulo" value="<?php echo $lupulo_data->id_lupulo; ?>">
             <div class="form-group">
-                <label for="nombre_nuevo_lupulo">Nombre</label>
-                <input type="text" class="form-control" id="nombre_nuevo_lupulo" name="nombre_nuevo_lupulo">
+                <label for="nombre_editar_lupulo">Nombre</label>
+                <input type="text" class="form-control" id="nombre_editar_lupulo" name="nombre_editar_lupulo" value="<?php echo $lupulo_data->nombre_lupulo; ?>">
             </div>
             <div class="form-group">
-                <label for="alfaacidos_nuevo_lupulo">Alfa Ácidos</label>
-                <input type="text" class="form-control" id="alfaacidos_nuevo_lupulo" name="alfaacidos_nuevo_lupulo">
+                <label for="alfaacidos_editar_lupulo">Alfa Ácidos</label>
+                <input type="text" class="form-control" id="alfaacidos_editar_lupulo" name="alfaacidos_editar_lupulo" value="<?php echo $lupulo_data->alfa_acidos; ?>">
             </div>
             <div class="form-group">
-                <button type="button" class="btn btn-default" onclick="saveLupulo(this.form)">Guardar</button>
+                <button type="button" class="btn btn-default" onclick="updateLupulo(this.form)">Guardar</button>
                 <button type="button" class="btn btn-checkout" onclick="window.location.href='?c=lupulo'">Cancelar</button>
             </div>
         </form>
